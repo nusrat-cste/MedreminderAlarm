0c7bc83ed889eddf25bcb356a47ca381291644be
chowdhurynusrat <nusrat.cste@gmail.com>
2018-10-18 23:31:46 -0400

    multiple alarm nonworking version

--- MedReminder/app/src/main/java/com/example/nusra/medreminder/AddAMedicineInfo.java
+++ MedReminder/app/src/main/java/com/example/nusra/medreminder/AddAMedicineInfo.java
@@ -1,17 +1,82 @@
 package com.example.nusra.medreminder;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static java.util.Calendar.getInstance;
+
 public class AddAMedicineInfo extends AppCompatActivity {
+
+    PendingIntent pending_intent;
+    AlarmManager alarm_manager;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_amedicine_info);
+
+//        db.deleteAll();
+
+        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        final Calendar calendar = getInstance();
+        this.context = this;
+        Intent my_intent = new Intent(this.context, Alarm_Receiver.class);
+//        sendng hardcoded alarm paprameters to store inside DB.
+          hardCodedAlarmDataPush();
+
+//        To get all the alarms stored in the DB
+        ArrayList<AnAlarmTask> allAlarmTasks = db.getAllAlarmTasks();
+
+//checks if the db is not null, and if not, it prints all the records
+        /*if (allAlarmTasks != null) {
+            int id = 0;
+
+            for (AnAlarmTask Task:allAlarmTasks) {
+
+                Log.e("DBdata",Task.getAlarmTaskName()+" "+Task.alarmTaskId);
+
+                calendar.set(Calendar.HOUR_OF_DAY, Task.getAlarmSetHour());
+                calendar.set(Calendar.MINUTE, Task.getAlarmSetMinutes());
+                calendar.set(Calendar.SECOND, Task.getAlarmSetSeconds());
+                calendar.set(Calendar.MILLISECOND, Task.getAlarmSetMilliSec());
+
+                long time = calendar.getTimeInMillis();
+
+                Log.e("Alarm", "Time" + String.valueOf(time));
+
+                pending_intent = PendingIntent.getBroadcast(getApplicationContext(), id,
+                        my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                //set the alarm manager
+                Log.e("Alarm","Starting alarm manager");
+                alarm_manager.set(AlarmManager.RTC_WAKEUP, time, pending_intent);
+                id++;
+            }
+        }
+        else
+            Log.e("DBdata","there is no records there");*/
     }
 
+    private void hardCodedAlarmDataPush() {
+        //        Manually saving alarms
+        AnAlarmTask t1 = new AnAlarmTask("Viodin", 1, "Once", "", "10/01/2018", 11, 54);
+        AnAlarmTask t2 = new AnAlarmTask("Histamin", 1, "Multiple", "Mon,Fri,Sat", "10/01/2018", 11, 57);
+        AnAlarmTask t3 = new AnAlarmTask("Napa", 1, "Once", "Mon,Fri", "10/01/2018", 12, 45);
+        AnAlarmTask t4 = new AnAlarmTask("Napa", 1, "Once", "Mon,Fri", "10/01/2018", 12, 50);
+        db.addAnAlarmTask(t1);
+        db.addAnAlarmTask(t2);
+        db.addAnAlarmTask(t3);
+        db.addAnAlarmTask(t4);
+    }
     public void onCheckboxClicked(View view) {
         // Is the view now checked?
 //        boolean checked = ((CheckBox) view).isChecked();
--- MedReminder/app/src/main/java/com/example/nusra/medreminder/MainActivity.java
+++ MedReminder/app/src/main/java/com/example/nusra/medreminder/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -23,80 +24,27 @@ import static java.util.Calendar.getInstance;
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabaseHandler db;
-    PendingIntent pending_intent;
-    AlarmManager alarm_manager;
-    Context context;
-
-
 
     @TargetApi(Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        db = new SQLiteDatabaseHandler(this);
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        final Calendar calendar = getInstance();
+//        need to capture the extra db object
 
-        this.context = this;
-
-        Intent my_intent = new Intent(this.context, Alarm_Receiver.class);
-
-//        Manually saving alarms
-        AnAlarmTask t1 = new AnAlarmTask("Viodin", 1, "Once", "", "10/01/2018", 11, 54);
-        AnAlarmTask t2 = new AnAlarmTask("Histamin", 1, "Multiple", "Mon,Fri,Sat", "10/01/2018", 11, 57);
-        AnAlarmTask t3 = new AnAlarmTask("Napa", 1, "Once", "Mon,Fri", "10/01/2018", 12, 45);
-        AnAlarmTask t4 = new AnAlarmTask("Napa", 1, "Once", "Mon,Fri", "10/01/2018", 12, 50);
-
-//        db.addAnAlarmTask(t1);
-//        db.addAnAlarmTask(t2);
-//        db.addAnAlarmTask(t3);
-//        db.addAnAlarmTask(t4);
-//        To get all the alarms stored in the DB
-
-        ArrayList<AnAlarmTask> allAlarmTasks = db.getAllAlarmTasks();
-
-        db.deleteAll();
-//checks if the db is not null, and if not, it prints all the records
-        if (allAlarmTasks != null) {
-            int id = 0;
-
-            for (AnAlarmTask Task:allAlarmTasks) {
-
-                Log.e("DBdata",Task.getAlarmTaskName()+" "+Task.alarmTaskId);
-
-                calendar.set(Calendar.HOUR_OF_DAY, Task.getAlarmSetHour());
-                calendar.set(Calendar.MINUTE, Task.getAlarmSetMinutes());
-                calendar.set(Calendar.SECOND, Task.getAlarmSetSeconds());
-                calendar.set(Calendar.MILLISECOND, Task.getAlarmSetMilliSec());
-
-                long time = calendar.getTimeInMillis();
-
-                Log.e("Alarm", "Time" + String.valueOf(time));
-
-                pending_intent = PendingIntent.getBroadcast(MainActivity.this, id,
-                        my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                //set the alarm manager
-                Log.e("Alarm","Starting alarm manager");
-                alarm_manager.set(AlarmManager.RTC_WAKEUP, time, pending_intent);
-                id++;
-            }
-        }
-        else
-            Log.e("DBdata","there is no records there");
-
-
+        db = new SQLiteDatabaseHandler(this);
+        final Intent intent = new Intent(getApplicationContext(), AddAMedicineInfo.class);
+        intent.putExtra("sql_obj", (Parcelable) db);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_new_alarm);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), AddAMedicineInfo.class));
+                startActivity(intent);
             }
         });
     }
